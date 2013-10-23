18dbdabff22cb59f3576463f38f24e74c738a00d
artem <seonetartem@gmail.com>
2013-10-23 23:56:43 +0300

    first commit

---
+++ first/first/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  first
+//
+//  Created by MaestroArt on 10/23/13.
+//  Copyright (c) 2013 MaestroArt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
